fc590baf8b1fed343b09ce45f099113287995caf
Federico Ragusa <federico.ragusa@accenture.com>
2025-04-12 12:16:09 +0200

    FedericoR: update env position

--- lib/src/core/env_config/env_config.dart
+++ lib/src/core/env_config/env_config.dart
@@ -2,7 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class EnvConfig {
   static Future<void> init() async {
-    await dotenv.load();
+    await dotenv.load(fileName: 'lib/resources/.env');
   }
 
   static String get currencyApiKey {
